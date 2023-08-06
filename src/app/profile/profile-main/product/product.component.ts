9bbb208c7d132e1b6eda28bbdd02cc09659e4685
karimul islam <7mdkarimul@gmail.com>
2023-08-06 09:05:04 -0800

    commit after implement tabs in product page

--- src/app/profile/profile-main/product/product.component.ts
+++ src/app/profile/profile-main/product/product.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+  public selectedTabs:string = "tab1";
+
+  public pricing:boolean = true;
+  public restock:boolean = false;
+  public shipping:boolean = false;
+  public global_delivery:boolean = false;
+  public attrubutes:boolean = false;
+  public advanced:boolean = false;
+  public addfeatured:boolean = false;
+  navigate_inventory(nav_type:string){
+
+    switch(nav_type){
+      case nav_type : {
+        alert(nav_type);
+      }
+      // case "pricing" : {
+      //   this.pricing = true;
+      // }
+    }
+
+  }
+
 }
